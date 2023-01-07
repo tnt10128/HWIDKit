2961500ffce330b5b884ac690f31f1659a88d02d
TNT10128 <tntthenoob@gmail.com>
2023-01-07 08:56:16 -0500

    Add more descriptive Javadoc comments everywhere

--- src/main/java/xyz/playneptune/hwidkit/HwidKit.java
+++ src/main/java/xyz/playneptune/hwidkit/HwidKit.java
@@ -5,13 +5,39 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Arrays;
 
+/**
+ * The HwidKit class provides a set of methods for generating unique identifiers for user devices.
+ * These identifiers can be used to identify devices in a variety of contexts, such as for tracking
+ * application usage or for software licensing.
+ *
+ * <p>The HwidKit class supports generating identifiers based on various specifications, like
+ * the operating system or core count. It also supports generating hashes of the generated
+ * identifiers using the MD5 algorithm, for added security.
+ *
+ * <p>To use the HwidKit class, users can pass in the desired specifications for the identifier
+ * using the {@link SystemSpecification} enum. The HwidKit class will then generate a unique 
+ * identifier based on these specifications, and return it as a string.
+ *
+ * <p>For example:
+ *
+ * <pre>
+ * HwidKit hwidKit = new HwidKit();
+ * String identifier = hwidKit.generateIdentifier(
+ *     SystemSpecification.OS_NAME,
+ *     SystemSpecification.OS_VERSION,
+ *     SystemSpecification.AVAILABLE_PROCESSORS
+ * );
+ * </pre>
+ * @author TNT10128
+ */
 public class HwidKit {
 
     /**
-     * Generates a unique identifier string based on the specs passed in
-     * and hashes it with the MD5 algorithm.
-     * @param specsToInclude The specs to include in the identifier.
-     * @return The hashed generated identifier.
+     * Generates a unique identifier string based on the given system specifications.
+     * The generated identifier is not hashed.
+     *
+     * @param specsToInclude The system specifications to include in the identifier.
+     * @return The unique identifier string.
      */
     public String generateIdentifier(SystemSpecification... specsToInclude) {
         String rawIdentifier = generateRawIdentifier(specsToInclude);
@@ -22,6 +48,7 @@ public class HwidKit {
     /**
      * Generates a unique identifier string based on the specs passed in.
      * This method does not hash the generated string.
+     * 
      * @param specsToInclude The specs to include in the identifier.
      * @return The unhashed generated identifier.
      */
@@ -35,8 +62,9 @@ public class HwidKit {
 
     /**
      * Generates a hash of the given string using the MD5 algorithm.
-     * @param toGetHashOf The string to get the hash of.
-     * @return The hash of the given string.
+     *
+     * @param toGetHashOf The string to generate a hash for.
+     * @return The MD5 hash of the given string.
      */
     public String getMd5HashOfString(String toGetHashOf) {
         try {
--- src/main/java/xyz/playneptune/hwidkit/SystemSpecification.java
+++ src/main/java/xyz/playneptune/hwidkit/SystemSpecification.java
@@ -1,17 +1,60 @@
 package xyz.playneptune.hwidkit;
 
+/**
+ * The SystemSpecification enum represents various system specifications that can be used to generate
+ * unique identifiers for user devices. Each enum value corresponds to a specific system specification,
+ * such as the operating system name or the system core count.
+ *
+ * <p>To generate an identifier using the SystemSpecification enum, users can pass one or more enum
+ * values to the {@link HwidKit#generateIdentifier(SystemSpecification...)} method. The HwidKit class
+ * will then generate a unique identifier based on the specified system specifications.
+ *
+ * <p>For example:
+ *
+ * <pre>
+ * HwidKit hwidKit = new HwidKit();
+ * String identifier = hwidKit.generateIdentifier(
+ *     SystemSpecification.OS_NAME,
+ *     SystemSpecification.OS_VERSION,
+ *     SystemSpecification.USER_NAME
+ * );
+ * </pre>
+ * @author TNT10128
+ */
 public enum SystemSpecification {
     
+    /** The operating system name. */
     OS_NAME(System.getProperty("os.name")),
+
+    /** The operating system architecture. */
     OS_ARCHITECTURE(System.getProperty("os.arch")),
+
+    /** The operating system version. */
     OS_VERSION(System.getProperty("os.version")),
+
+    /** The user's name. */
     USER_NAME(System.getProperty("user.name")),
+
+    /** The number of available processors on the system. */
     AVAILABLE_PROCESSORS(String.valueOf(Runtime.getRuntime().availableProcessors())),
+
+    /** The processor identifier for Windows systems. */
     WIN_PROCESSOR_IDENTIFIER(System.getenv("PROCESSOR_IDENTIFIER")),
+
+    /** The processor architecture for Windows systems. */
     WIN_PROCESSOR_ARCHITECTURE(System.getenv("PROCESSOR_ARCHITECTURE")),
+
+    /** The processor architecture for 64-bit Windows systems. */
     WIN_PROCESSOR_ARCHITEW6432(System.getenv("PROCESSOR_ARCHITEW6432")),
+
+    /** The number of processors for Windows systems. */
     WIN_NUMBER_OF_PROCESSORS(System.getenv("NUMBER_OF_PROCESSORS"));
 
+    /**
+     * The string representation of this system specification.
+     * This field is initialized with the value of the system 
+     * specification when the enum value is created.
+     */
     public final String asString;
 
     SystemSpecification(String asString) {
